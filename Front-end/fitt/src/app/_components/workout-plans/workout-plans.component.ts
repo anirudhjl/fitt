17f256c483cfa3383379eb0320e51ec50b5f5b8a
anirudhjl1 <anirudh.jl@lntinfotech.com>
2021-08-24 18:47:14 +0530

    Added reminder functionality

--- Front-end/fitt/src/app/_components/workout-plans/workout-plans.component.ts
+++ Front-end/fitt/src/app/_components/workout-plans/workout-plans.component.ts
@@ -5,6 +5,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./workout-plans.component.css'],
 })
 export class WorkoutPlansComponent implements OnInit {
+  token = atob(localStorage.getItem('token') || '');
+  username = this.token?.split(':')[0];
+  href: string;
+
   constructor() {}
+
   ngOnInit(): void {}
+
+  sendMail() {
+    const mailto: string =
+      'mailto:' +
+      this.username +
+      '?subject=FITT%20reminder%20&body=Hi,%0d%0aJust%20a%20friendly%20reminder%20to%20workout%20and%20stay%20fit.';
+    window.location.href = mailto;
+  }
 }
