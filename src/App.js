05c48dbc187e1101023c43587cc4353e0b5e3056
anirudhjl <anibhat022@gmail.com>
2022-01-17 19:30:19 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Meet Doctor",
+      date: "Feb 4th 2:30 PM",
+    },
+    {
+      id: 2,
+      text: "Meet John",
+      date: "Feb 5th 12:30 PM",
+    },
+    {
+      id: 3,
+      text: "Go Shopping",
+      date: "Feb 6th 10:30 AM",
+    },
+  ]);
+
+  const addTask = (task) => {
+    console.log(task);
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        <p>No tasks to show. Consider adding tasks.</p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
